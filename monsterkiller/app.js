78432ef37581e984a41ef2a8d232e3bd2b689a18
leslaron <lord.darg@gmail.com>
2019-02-15 18:05:45 +0100

    monsterkiller animations added. more coloring on log

--- monsterkiller/app.js
+++ monsterkiller/app.js
@@ -6,38 +6,42 @@ new Vue({
         monsterHealth: 100,
         history: [],        
         cooldownTimer: 0,
+        turnCount: 0
     },
     
     methods: {
         attack() {
+            this.turnCount++
             this.monsterAttack()
             const playerHit = this.calculateEffect(3, 10)          
             this.monsterHealth -= playerHit    
-            this.history.unshift({isPlayer: true, text:"Player hits monster for " + playerHit})
+            this.history.unshift({isPlayer: true, text:"Player hits monster for " + playerHit, id: this.turnCount})
             this.cooldownTimer -= 1
             this.checkStatus()
         },
         specialAttack(){
+            this.turnCount++
             if(!this.onCooldown){
                 this.monsterAttack()
                 const playerHit = this.calculateEffect(10, 20)          
                 this.monsterHealth -= playerHit    
-                this.history.unshift({isPlayer: true, text:"Player unleashes their fury for " + playerHit + " damage!"})
+                this.history.unshift({isPlayer: true, text:"Player unleashes their fury for " + playerHit + " damage!", id: this.turnCount, special: true})
                 this.cooldownTimer = 5
                 this.checkStatus()
             }
         },
         heal() {
+            this.turnCount++
             this.monsterAttack()
             this.checkStatus()
             if(!this.mainMenu){
                 const playerHeal = this.calculateEffect(5, 30)
                 if(this.playerHealth + playerHeal >= 100){
-                    this.history.unshift({isPlayer: true, text:"Player heals for " + (100 - this.playerHealth)})
+                    this.history.unshift({isPlayer: true, text:"Player heals for " + (100 - this.playerHealth), id: this.turnCount, isHeal: true})
                     this.playerHealth = 100
                 } else {
                     this.playerHealth += playerHeal   
-                    this.history.unshift({isPlayer: true, text:"Player heals for " + playerHeal})
+                    this.history.unshift({isPlayer: true, text:"Player heals for " + playerHeal, id: this.turnCount, isHeal: true})
                 }
                 this.cooldownTimer -= 1
             }
@@ -52,7 +56,7 @@ new Vue({
         monsterAttack(){
             const monsterHit = this.calculateEffect(5, 20)
             this.playerHealth -= monsterHit
-            this.history.unshift({isPlayer: false, text:"Monster hits player for " + monsterHit})
+            this.history.unshift({isPlayer: false, text:"Monster hits player for " + monsterHit, id: this.turnCount})
         },
         checkStatus(){
             if(this.monsterHealth <= 0){
